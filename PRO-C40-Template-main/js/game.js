74bdc1ee20057345bcc2feacb0f36b71158ac07b
122335 <78242308+122335@users.noreply.github.com>
2022-01-25 19:26:14 +1100

    Add files via upload

--- PRO-C40-Template-main/js/game.js
+++ PRO-C40-Template-main/js/game.js
@@ -53,7 +53,7 @@ class Game {
         fill("red");
         textSize(20);
         // add code to display the player name on the respective basket
-        text(player.name)
+        text(allPlayers[plr].name, x - 25, y + 25)
       }
 
       textSize(25);
